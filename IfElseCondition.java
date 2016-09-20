486655f7c777101cb3478244fa76b3ca58c4b395
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 06:43:50 -0400

    Create IfElseCondition.java

---
+++ IfElseCondition.java
@@ -0,0 +1,15 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        int age = 30;
+        if (age < 16) 
+        {
+            System.out.println("Too Young");
+        } 
+        else 
+        {
+            System.out.println("Welcome!");
+        }
+    }
+}
