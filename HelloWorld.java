be9a1b9b965cca2ae5eb0fe5a40b50f326db97bd
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 06:31:36 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class MyClass {
+  public static void main(String[ ] args) {
+    System.out.println("Hello World");
+  }
+}
+/*
+-public: anyone can access it
+-static: method can be run without creating an instance of the class containing the main method
+-void: method doesn't return any value
+-main: the name of the method
+In summary:
+-Every program in Java must have a class.
+-Every Java program starts from the main method.
+*/
