149e19113ed698e300f4907d978df9c79d55f56b
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 06:39:35 -0400

    Update ReadInput.java

--- ReadInput.java
+++ ReadInput.java
@@ -1,9 +1,10 @@
 //Import scanner utility
 import java.util.Scanner;
 
-
-class MyClass {
-    public static void main(String[ ] args) {
+class MyClass 
+{
+    public static void main(String[ ] args) 
+    {
         //read line
         Scanner myVar = new Scanner(System.in);
         //print line
