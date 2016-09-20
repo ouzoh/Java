79df5f5d827d6717e8b9b2f7f77fd26a6f0a3457
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 06:38:27 -0400

    Create IfCondition.java

---
+++ IfCondition.java
@@ -0,0 +1,11 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        int x = 7;
+        if (x < 42) 
+        {
+            System.out.println("Hi");
+        }
+    }
+}
