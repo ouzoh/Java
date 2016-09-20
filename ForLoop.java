3590fddfcefe78e4154dc65aacaeadf60c13d0b3
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 17:55:07 -0400

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,20 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        for(int x=0; x<=10; x=x+2) 
+        {
+            System.out.println(x);
+        }
+    }
+}
+
+
+/*
+Efficient way of writing loop
+For loop syntax
+for (initialization; condition; increment/decrement) 
+{
+   statement(s)
+}
+*/
