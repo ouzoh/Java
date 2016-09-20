3ce35aafe1825bd90f300ae1c0f9d8f37c6f6e5a
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 17:47:28 -0400

    Create WhileLoop.java

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        int x = 3;
+        while(x > 0) //condition
+        {
+            System.out.println(x);
+            x=x-1; //decrease step by 1
+        }
+    }
+}
