88d625725e32a9a1607ad3e98692e6a84530e695
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 06:17:56 -0400

    Create readinput.java

---
+++ readinput.java
@@ -0,0 +1,12 @@
+//Import scanner utility
+import java.util.Scanner;
+
+
+class MyClass {
+    public static void main(String[ ] args) {
+        //read line
+        Scanner myVar = new Scanner(System.in);
+        //print line
+        System.out.println(myVar.nextLine());        
+    }
+}
