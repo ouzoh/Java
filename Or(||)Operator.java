a6ce2059c5e1813314c9764f2ec528dec136ec38
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-20 07:02:04 -0400

    Create Or(||)Operator.java

---
+++ Or(||)Operator.java
@@ -0,0 +1,14 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        int age = 25;
+        int money = 100;
+        
+        // || is Or
+        if (age > 18 || money > 500) 
+        {
+            System.out.println("Welcome!");
+        }
+    }
+}
