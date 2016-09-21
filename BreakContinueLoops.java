acdf9a482d8e601bc70ffccf250ce0cf966e99c8
Ogaga Uzoh <friendsofglory@gmail.com>
2016-09-21 18:20:43 -0400

    Create BreakContinueLoops.java

---
+++ BreakContinueLoops.java
@@ -0,0 +1,45 @@
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        int x = 1;
+        while(x > 0) 
+        {
+            System.out.println(x);
+            if(x == 4) 
+            {
+                break;
+            }
+            x++;
+        }
+    }
+}
+
+/*Break statement terminates the loop and 
+executes the statement immediately following the loop.
+
+Similar to exit in fortran
+*/
+
+
+public class Program 
+{
+    public static void main(String[] args) 
+    {
+        for(int x=10; x<=40; x=x+10) 
+        {
+            if(x == 30) 
+            {
+                continue;
+            }
+            System.out.println(x);
+        }
+    }
+}
+
+/*
+The continue statement causes the loop to skip the remainder of its body and 
+then immediately retest its condition prior to reiterating
+
+similar to continue in fortran
+*/
